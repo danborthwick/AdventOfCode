7b9132896de3a3f13c56e26c96dea52cf1bb6904
Dan Borthwick <danborthwick@hotmail.com>
2015-12-07 19:31:45 +0000

    Day6

---
+++ src/Day6Lights.java
@@ -0,0 +1,87 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+public class Day6Lights {
+    public static int countLights() throws Exception {
+        FileInputStream fstream = new FileInputStream("Day6Input.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+        String strLine;
+        boolean[][] lights = new boolean[1000][1000];
+        for (boolean[] row : lights) {
+            for (int x =0; x < row.length; x++) {
+                row[x] = false;
+            }
+        }
+
+        while ((strLine = br.readLine()) != null)   {
+            apply(strLine, lights);
+        }
+
+        return countOn(lights);
+    }
+
+    private static int countOn(boolean[][] lights) {
+        int result = 0;
+        for (boolean[] row : lights) {
+            for (boolean light : row) {
+                if (light) result++;
+            }
+        }
+
+        return result;
+    }
+
+    enum Instruction {
+        ON, OFF, TOGGLE
+    }
+
+    public static Instruction fromString(String s) {
+        switch (s) {
+            case "on":
+                return Instruction.ON;
+            case "off":
+                return Instruction.OFF;
+            case "toggle":
+                return Instruction.TOGGLE;
+        }
+        return Instruction.TOGGLE;
+    }
+
+
+    private static void apply(String instruction, boolean[][] lights) {
+        String[] parts = instruction.split("[ ,]");
+        int  offset = parts[0].equals("turn") ? 1 : 0;
+        int x0 = Integer.valueOf(parts[offset + 1]);
+        int y0 = Integer.valueOf(parts[offset + 2]);
+        int x1 = Integer.valueOf(parts[offset + 4]);
+        int y1 = Integer.valueOf(parts[offset + 5]);
+
+        Instruction in = fromString(parts[offset]);
+        for (int x = x0; x <= x1; x++) {
+            for (int y=y0; y <= y1; y++) {
+                applyLight(x, y, lights, in);
+            }
+        }
+    }
+
+    private static void applyLight(int x, int y, boolean[][] lights, Instruction in) {
+        switch (in) {
+            case ON:
+                lights[y][x] = true;
+                break;
+            case OFF:
+                lights[y][x] = false;
+                break;
+            case TOGGLE:
+                lights[y][x] = !lights[y][x];
+                break;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Lights: " + countLights());
+    }
+}
---
+++ src/Day6Lights2.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Day6Lights2 {
+    public static int countLights() throws Exception {
+        FileInputStream fstream = new FileInputStream("Day6Input.txt");
+        BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+
+        String strLine;
+        int[][] lights = new int[1000][1000];
+
+        while ((strLine = br.readLine()) != null)   {
+            apply(strLine, lights);
+        }
+
+        return countOn(lights);
+    }
+
+    private static int countOn(int[][] lights) {
+        int result = 0;
+        for (int[] row : lights) {
+            for (int light : row) {
+                result += light;
+            }
+        }
+
+        return result;
+    }
+
+    enum Instruction {
+        ON, OFF, TOGGLE
+    }
+
+    public static Instruction fromString(String s) {
+        switch (s) {
+            case "on":
+                return Instruction.ON;
+            case "off":
+                return Instruction.OFF;
+            case "toggle":
+                return Instruction.TOGGLE;
+        }
+        return Instruction.TOGGLE;
+    }
+
+
+    private static void apply(String instruction, int[][] lights) {
+        String[] parts = instruction.split("[ ,]");
+        int  offset = parts[0].equals("turn") ? 1 : 0;
+        int x0 = Integer.valueOf(parts[offset + 1]);
+        int y0 = Integer.valueOf(parts[offset + 2]);
+        int x1 = Integer.valueOf(parts[offset + 4]);
+        int y1 = Integer.valueOf(parts[offset + 5]);
+
+        Instruction in = fromString(parts[offset]);
+        for (int x = x0; x <= x1; x++) {
+            for (int y=y0; y <= y1; y++) {
+                applyLight(x, y, lights, in);
+            }
+        }
+    }
+
+    private static void applyLight(int x, int y, int[][] lights, Instruction in) {
+        switch (in) {
+            case ON:
+                lights[y][x]++;
+                break;
+            case OFF:
+                lights[y][x] = Math.max(lights[y][x] - 1, 0);
+                break;
+            case TOGGLE:
+                lights[y][x] += 2;
+                break;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Light2s: " + countLights());
+    }
+}
