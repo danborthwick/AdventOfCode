855b6726533769286313e8c24fcecd2057cbdb79
Dan Borthwick <danborthwick@hotmail.com>
2015-12-20 13:49:08 +0000

    Day20 Done

--- src/Day20FactorDeliveries.java
+++ src/Day20FactorDeliveries.java
@@ -1,31 +1,45 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 public class Day20FactorDeliveries {
 
     public static int minHouseNumberReceivingAtLeast(int minDeliveries) {
 
         minDeliveries /= 10;
-        List<Set<Integer>> factors = new ArrayList<>(minDeliveries + 1);
-        for (int i=0; i <= minDeliveries; i++) {
-            factors.add(new HashSet<>());
+        int[] factorSums = new int[minDeliveries + 1];
+        for (int i=2; i <= minDeliveries; i++) {
+            factorSums[i] = i + 1;
         }
 
         for (int p = 2; p < minDeliveries / 2; p++) {
             for (int i=p+p; i <= minDeliveries; i += p) {
-                factors.get(i).add(p);
+                factorSums[i] += p;
             }
         }
 
-        for (int i=1; i <= minDeliveries; i++) {
-            int factorSum = i + 1;  // Prime factors
-            Set<Integer> iFactors = factors.get(i);
-            for (Integer iFactor : iFactors) {
-                factorSum += iFactor;
+        for (int i=2; i < minDeliveries; i++) {
+            if (factorSums[i] >= minDeliveries) {
+                return i;
             }
-            if (factorSum >= minDeliveries) {
+        }
+
+        return -1;
+    }
+
+    public static int minHouseNumberReceivingAtLeastB(int minDeliveries) {
+
+        minDeliveries /= 11;
+        int[] factorSums = new int[minDeliveries + 1];
+        for (int i=2; i <= minDeliveries; i++) {
+            factorSums[i] = i;
+        }
+
+        for (int p = 1; p < minDeliveries / 2; p++) {
+            int iMax = Math.min(p * 50, minDeliveries);
+            for (int i=p+p; i <= iMax; i += p) {
+                factorSums[i] += p;
+            }
+        }
+
+        for (int i=2; i < minDeliveries; i++) {
+            if (factorSums[i] >= minDeliveries) {
                 return i;
             }
         }
@@ -35,6 +49,7 @@ public class Day20FactorDeliveries {
 
     public static void main(String[] args) {
         System.out.println("A: " + Day20FactorDeliveries.minHouseNumberReceivingAtLeast(36000000));
+        System.out.println("B: " + Day20FactorDeliveries.minHouseNumberReceivingAtLeastB(36000000));
     }
 
 }
