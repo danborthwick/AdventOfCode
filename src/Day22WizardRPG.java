7447634d38fed1e60c6395585c09247ec7644a96
Dan Borthwick <danborthwick@hotmail.com>
2015-12-23 10:30:19 +0000

    Day22 working

--- src/Day22WizardRPG.java
+++ src/Day22WizardRPG.java
@@ -1,5 +1,12 @@
 public class Day22WizardRPG {
 
+    int minFound = Integer.MAX_VALUE;
+    boolean hardMode;
+
+    public Day22WizardRPG(boolean hardMode) {
+        this.hardMode = hardMode;
+    }
+
     enum Spell {
         MagicMissile, Drain, Shield, Poison, Recharge;
 
@@ -15,91 +22,147 @@ public class Day22WizardRPG {
         public int bossDamage;
         public int playerHitPoints;
         public int mana;
+        public int manaSpent = 0;
         public int shieldRemaining = 0;
         public int poisonRemaining = 0;
         public int rechargeRemaining = 0;
 
-        public boolean isSpellActive(Spell spell) {
-            return (spell == Spell.Shield && shieldRemaining > 0)
-                    || (spell == Spell.Poison && poisonRemaining > 0)
-                    || (spell == Spell.Recharge && rechargeRemaining > 0);
+        public GameState(int bossHitPoints, int bossDamage, int playerHitPoints, int mana) {
+            this.bossHitPoints = bossHitPoints;
+            this.bossDamage = bossDamage;
+            this.playerHitPoints = playerHitPoints;
+            this.mana = mana;
+        }
+
+        public boolean canCast(Spell spell) {
+
+            if (spell.cost() > mana)
+                return false;
+
+            switch (spell) {
+                case Shield: return shieldRemaining <= 1;
+                case Poison: return poisonRemaining <= 1;
+                case Recharge: return rechargeRemaining <= 1;
+                default: return true;
+            }
+        }
+
+        private void cast(Spell spellToCast) {
+            switch (spellToCast) {
+                case MagicMissile:
+                    bossHitPoints -= 4;
+                    break;
+
+                case Drain:
+                    bossHitPoints -= 2;
+                    playerHitPoints += 2;
+                    break;
+
+                case Shield:
+                    shieldRemaining = 6;
+                    break;
+
+                case Poison:
+                    poisonRemaining = 6;
+                    break;
+
+                case Recharge:
+                    rechargeRemaining = 5;
+                    break;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            GameState gameState = (GameState) o;
+
+            if (bossHitPoints != gameState.bossHitPoints) return false;
+            if (bossDamage != gameState.bossDamage) return false;
+            if (playerHitPoints != gameState.playerHitPoints) return false;
+            return mana == gameState.mana;
+
+        }
+
+        @Override
+        public int hashCode() {
+            int result = bossHitPoints;
+            result = 31 * result + bossDamage;
+            result = 31 * result + playerHitPoints;
+            result = 31 * result + mana;
+            return result;
         }
 
         @Override
         public Object clone() throws CloneNotSupportedException {
             return super.clone();
         }
+
+        public boolean playerDead() {
+            return playerHitPoints <= 0;
+        }
     }
 
-    public static int leastManaToWin() throws Exception {
-
-        GameState state = new GameState();
-        state.bossHitPoints = 58;
-        state.bossDamage = 9;
-        state.playerHitPoints = 50;
-        state.mana = 500;
-
-        return helper(state, 0);
+    public int leastManaToWin(GameState state) throws Exception {
+        minFound = Integer.MAX_VALUE;
+        leastManaToWinHelper(state);
+        return minFound;
     }
 
-    private static int helper(GameState oldState, int oldManaSpent) throws Exception {
-
-        applyEffectsReturnArmour(oldState);
-        int minManaSpent = Integer.MAX_VALUE;
-
+    private void leastManaToWinHelper(GameState oldState) throws Exception {
         for (Spell spellToCast : Spell.values()) {
 
-            if (oldState.isSpellActive(spellToCast)) {
+            if (!oldState.canCast(spellToCast))
                 continue;
+
+            GameState newState = stepBattleWithPlayerSpell(oldState, spellToCast);
+
+            if (newState.bossHitPoints <= 0) {
+                if (newState.manaSpent < minFound) {
+                    minFound = newState.manaSpent;
+                }
             }
-
-            GameState newState = (GameState) oldState.clone();
-            newState.mana -= spellToCast.cost();
-            int newManaSpent = oldManaSpent + spellToCast.cost();
-
-            switch (spellToCast) {
-                case MagicMissile:
-                    newState.bossHitPoints -= 4;
-                    break;
-
-                case Drain:
-                    newState.bossHitPoints -= 2;
-                    newState.playerHitPoints += 2;
-                    break;
-
-                case Shield:
-                    newState.shieldRemaining = 6;
-                    break;
-
-                case Poison:
-                    newState.poisonRemaining = 6;
-                    break;
-
-                case Recharge:
-                    newState.rechargeRemaining = 5;
-                    break;
+            else if (!newState.playerDead()
+                    && (newState.manaSpent < minFound)) {
+                leastManaToWinHelper(newState);
             }
-
-
-            // Boss Turn
-            int armour = applyEffectsReturnArmour(newState);
-           if (newState.bossHitPoints <= 0) {
-                return newManaSpent;
-            }
-
-            newState.playerHitPoints -= Math.max(newState.bossDamage - armour, 1);
-
-            if (newState.playerHitPoints <= 0) {
-                return Integer.MAX_VALUE;
-            }
-
-            minManaSpent = Math.min(minManaSpent, helper(newState, newManaSpent));
         }
-
-        return minManaSpent;
     }
 
-    private static int applyEffectsReturnArmour(GameState oldState) {
+    public GameState stepBattleWithPlayerSpell(final GameState oldState, Spell playerSpell) throws Exception {
+        GameState newState = (GameState) oldState.clone();
+
+        if (hardMode) {
+            newState.playerHitPoints--;
+            if (newState.playerDead()) {
+                return newState;
+            }
+        }
+
+        applyEffectsReturnArmour(newState);
+
+        newState.mana -= playerSpell.cost();
+        newState.manaSpent = oldState.manaSpent + playerSpell.cost();
+
+        // Terminate this branch of depth first search if we already have a better answer
+        if (newState.manaSpent >= minFound) {
+            return newState;
+        }
+
+        newState.cast(playerSpell);
+
+        // Boss Turn
+        int armour = applyEffectsReturnArmour(newState);
+        if (newState.bossHitPoints > 0) {
+            newState.playerHitPoints -= Math.max(newState.bossDamage - armour, 1);
+        }
+
+        return newState;
+    }
+
+    private int applyEffectsReturnArmour(GameState oldState) {
         int armour = 0;
 
         if (oldState.shieldRemaining > 0) {
@@ -118,6 +181,8 @@ public class Day22WizardRPG {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("A: " + leastManaToWin());
+        GameState state = new GameState(58, 9, 50, 500);
+        System.out.println("Easy: " + new Day22WizardRPG(false).leastManaToWin(state));
+        System.out.println("Hard: " + new Day22WizardRPG(true).leastManaToWin(state));
     }
 }
---
+++ src/Day22WizardRPGTest.java
@@ -0,0 +1,53 @@
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class Day22WizardRPGTest {
+
+    @Test
+    public void testA1() throws Exception {
+        //suppose the player has 10 hit points and 250 mana, and that the boss has 13 hit points and 8 damage
+        Day22WizardRPG.GameState state = new Day22WizardRPG.GameState(13, 8, 10, 250);
+        int actual = new Day22WizardRPG(false).leastManaToWin(state);
+        assertThat(actual, is(226));
+    }
+
+    public Day22WizardRPG.GameState initialStateForA2() {
+        return new Day22WizardRPG.GameState(14, 8, 10, 250);
+    }
+
+    @Test @Ignore
+    public void testA2() throws Exception {
+        //suppose the player has 10 hit points and 250 mana, and that the boss has 13 hit points and 8 damage
+        //Now, suppose the same initial conditions, except that the boss has 14 hit points instead:
+        Day22WizardRPG.GameState state = initialStateForA2();
+        int actual = new Day22WizardRPG(false).leastManaToWin(state);
+        assertThat(actual, is(641));
+    }
+
+    @Test
+    public void testA2Verbose() throws Exception {
+        Day22WizardRPG.GameState state = initialStateForA2();
+        Day22WizardRPG rpg = new Day22WizardRPG(false);
+
+        state = rpg.stepBattleWithPlayerSpell(state, Day22WizardRPG.Spell.Recharge);
+        assertThat(state, is(new Day22WizardRPG.GameState(14, 8, 2, 122)));
+
+        state = rpg.stepBattleWithPlayerSpell(state, Day22WizardRPG.Spell.Shield);
+        assertThat(state, is(new Day22WizardRPG.GameState(14, 8, 1, 211)));
+
+        state = rpg.stepBattleWithPlayerSpell(state, Day22WizardRPG.Spell.Drain);
+        assertThat(state, is(new Day22WizardRPG.GameState(12, 8, 2, 340)));
+
+        state = rpg.stepBattleWithPlayerSpell(state, Day22WizardRPG.Spell.Poison);
+        assertThat(state, is(new Day22WizardRPG.GameState(9, 8, 1, 167)));
+
+        state = rpg.stepBattleWithPlayerSpell(state, Day22WizardRPG.Spell.MagicMissile);
+        assertThat(state, is(new Day22WizardRPG.GameState(-1, 8, 1, 114)));
+    }
+
+
+
+}
