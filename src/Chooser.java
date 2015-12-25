518b92ad0f9a32a67b741c856693c4cb1f20d7ef
Dan Borthwick <danborthwick@hotmail.com>
2015-12-25 12:50:50 +0000

    Chooser

---
+++ src/Chooser.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class Chooser<T> {
+    private final List<T> from;
+
+    public Chooser(List<T> from) {
+        this.from = from;
+    }
+
+    public void choose(final int n, Function<List<T>, Void> f) {
+
+        List<T> output = new ArrayList<>(n);
+
+        for (int i=0; i < n; i++) {
+            output.add(from.get(i));
+        }
+
+        helper(n, 0, output, f);
+    }
+
+    private void helper(int len, int startPosition, List<T> output, Function<List<T>, Void> f) {
+        if (len == 0) {
+            f.apply(output);
+        }
+        else {
+            for (int i = startPosition; i <= from.size() - len; i++) {
+                output.set(output.size() - len, from.get(i));
+                helper(len - 1, i + 1, output, f);
+            }
+        }
+    }
+
+    private long factorial(int n) {
+        long result = 1;
+        for (int i=2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
---
+++ src/ChooserTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+
+public class ChooserTest {
+
+
+    List<Character> abcList = Arrays.asList('a', 'b', 'c');
+
+    @Test
+    public void testChoose1() {
+
+        List<List<Character>> actual = new ArrayList<>();
+
+        new Chooser<>(abcList).choose(1, l -> { actual.add(new ArrayList<>(l)); return null; });
+
+        assertThat(actual, containsInAnyOrder(Arrays.asList('a'), Arrays.asList('b'), Arrays.asList('c')));
+    }
+
+    @Test
+    public void testChoose2() {
+
+        List<List<Character>> actual = new ArrayList<>();
+
+        new Chooser<>(abcList).choose(2, l -> { actual.add(new ArrayList<>(l)); return null; });
+
+        assertThat(actual, containsInAnyOrder(Arrays.asList('a', 'b'), Arrays.asList('b', 'c'), Arrays.asList('a', 'c')));
+    }
+}
