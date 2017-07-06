809ad570e018b4348530d7c62a04d2e73e92f41f
Dan Borthwick <danborthwick@hotmail.com>
2017-07-06 18:00:09 +1000

    Day 9 pt 2 close

--- src/y2016/Day9Decompression.java
+++ src/y2016/Day9Decompression.java
@@ -2,17 +2,21 @@ package y2016;
 
 import util.StringProvider;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 
 public class Day9Decompression {
+
+    public static final Pattern PATTERN = Pattern.compile("\\((\\d+)x(\\d+)\\)");
+
     public static int decompressedLength(String input) {
-        Pattern pattern = Pattern.compile("\\((\\d+)x(\\d+)\\)");
         int inputPosition = 0;
         StringBuilder output = new StringBuilder();
-        Matcher matcher = pattern.matcher(input);
+        Matcher matcher = PATTERN.matcher(input);
 
         while (matcher.find()) {
             if (matcher.start() < inputPosition) {
@@ -21,18 +25,77 @@ public class Day9Decompression {
 
             output.append(input, inputPosition, matcher.start());
 
-            int length = parseInt(matcher.group(1));
-            int repeats = parseInt(matcher.group(2));
-            String sequence = input.substring(matcher.end(), matcher.end() + length);
+            Match match = new Match(matcher);
+            String sequence = input.substring(match.sequenceStart, match.sequenceStart + match.length);
 
-            for (int r = 0; r < repeats; r++) {
+            for (int r = 0; r < match.repeats; r++) {
                 output.append(sequence);
             }
-            inputPosition = matcher.end() + length;
+            inputPosition = match.sequenceStart + match.length;
         }
         output.append(input, inputPosition, input.length());
         return output.length();
     }
+
+    static int decompressedLength2(String input) {
+        List<Match> matches = new ArrayList<>();
+        Matcher matcher = PATTERN.matcher(input);
+
+        while (matcher.find()) {
+            matches.add(new Match(matcher));
+        }
+
+        if (matches.isEmpty()) {
+            return input.length();
+        }
+        else {
+            return helper2(matches, 0, 0, input.length());
+        }
+    }
+
+    static int helper2(List<Match> matches, int matchIndex, int start, int end) {
+        int length = 0;
+        Match rootMatch = matches.get(matchIndex);
+        int position = rootMatch.sequenceStart;
+        length += (rootMatch.matchStart - start);  // Alpha characters before match
+
+        for (int m = matchIndex + 1; m < matches.size(); m++) {
+
+            Match match = matches.get(m);
+            if (match.sequenceStart < end) {
+                length += rootMatch.repeats * helper2(matches, m, position, rootMatch.sequenceEnd);
+                position = match.sequenceEnd;
+            }
+            else {
+                break;
+            }
+        }
+
+        if (position < rootMatch.sequenceEnd) {
+            length += (rootMatch.sequenceEnd - position) * rootMatch.repeats;
+            position = rootMatch.sequenceEnd;
+        }
+        length += end - position;
+
+        return length;
+    }
+
+    static class Match {
+        public int sequenceStart;
+        public int sequenceEnd;
+        public int matchStart;
+        public int length;
+        public int repeats;
+
+        public Match(Matcher matcher) {
+            length = parseInt(matcher.group(1));
+            repeats = parseInt(matcher.group(2));
+            matchStart = matcher.start();
+            sequenceStart = matcher.end();
+            sequenceEnd = sequenceStart + length;
+        }
+    }
+
 
     static int decompressedLength(StringProvider input) throws Exception {
         int length = 0;
@@ -42,7 +105,7 @@ public class Day9Decompression {
         return length;
     }
 
-    static int decompressedLength3(StringProvider input) throws Exception {
+    static int decompressedLength2(StringProvider input) throws Exception {
         int length = 0;
         while (input.hasMore()) {
             length += decompressedLength2(input.next());
--- src/y2016/Day9DecompressionTest.java
+++ src/y2016/Day9DecompressionTest.java
@@ -30,4 +30,21 @@ public class Day9DecompressionTest {
     public void test6() {
         assertThat(Day9Decompression.decompressedLength("X(8x2)(3x3)ABCY"), is(18));
     }
+
+    @Test
+    public void test21() {
+        assertThat(Day9Decompression.decompressedLength2("(3x3)XYZ"), is(9));
+    }
+    @Test
+    public void test22() {
+        assertThat(Day9Decompression.decompressedLength2("X(8x2)(3x3)ABCY"), is(20));
+    }
+    @Test
+    public void test23() {
+        assertThat(Day9Decompression.decompressedLength2("(27x12)(20x12)(13x14)(7x10)(1x12)A"), is(241920));
+    }
+    @Test
+    public void test24() {
+        assertThat(Day9Decompression.decompressedLength2("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"), is(445));
+    }
 }
